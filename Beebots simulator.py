2e29d3f35a03f243eef8ea5f4d256fee3b730db1
lwbjorklund <33005010+lwbjorklund@users.noreply.github.com>
2017-10-22 21:18:34 +0200

    2:nd

--- Beebots simulator.py
+++ Beebots simulator.py
@@ -5,32 +5,22 @@ import time
 import _thread
 
 # Contants
-UP_COLOR = 'blue'
-DOWN_COLOR = 'yellow'
-LEFT_COLOR = 'red'
-RIGHT_COLOR = 'green'
 BLINK_COLOR = 'white'
+UP = '00'
+LEFT = '01'
+RIGHT = '02'
+DOWN = '03'
 
 # Global variables
-LARGE_FONT = ("Verdana",20)
-UP = "00"
-DOWN = "03"
-LEFT = "01"
-RIGHT = "02"
+LARGE_FONT = ("Verdana",10)
 
 class _zo : pass
 zo = _zo()
 zo.a = {}
-zo.up_button = {}
-zo.down_button = {}
-zo.right_button = {}
-zo.left_button = {}
 zo.the_way_to_go = []
 zo.frame = None
 zo.color = "blue","red","green","yellow","orange","brown","dodgerblue","pink"
 zo.text = "/\\","<--","-->", "\\/"
-
-
 
 
 class App:
@@ -50,14 +40,7 @@ class App:
 		frame = tk.Frame (master, bg= 'red')
 		frame.pack(expand = 1, anchor = tk.S)
 		
-			
 		zo.frame = frame
-		'''
-		self.set_down_button (frame, 'on')
-		self.set_up_button (frame, 'on')
-		self.set_right_button (frame, 'on')
-		self.set_left_button (frame, 'on')
-		'''
 		self.do_buttons (2)
 		
 	def do_buttons (self, nmb):
@@ -69,101 +52,52 @@ class App:
 				 
 				butn = tk.Button(zo.frame, bg=zo.color[ind], font=LARGE_FONT, text = zo.text[ind])
 				if ind == 0:
-					butn.config(command = lambda: self.vilken_direction('00'))
-					butn.grid(column=0, row=0, sticky = tk.N, columnspan = 2, ipadx = 47)
+					butn.config(command = lambda: self.vilken_direction(UP))
+					butn.grid(column=0, row=0, sticky = tk.N, columnspan = 2)
 					
+					ws_up = butn.winfo_reqwidth()
 				elif ind == 1:
-					butn.config(command = lambda: self.vilken_direction('01'))
+					butn.config(command = lambda: self.vilken_direction(LEFT))
 					butn.grid(column=0, row=1, sticky = tk.W)
 					
+					
 				elif ind == 2:
-					butn.config(command = lambda: self.vilken_direction('02'))
+					butn.config(command = lambda: self.vilken_direction(RIGHT))
 					butn.grid(column=1, row=1, sticky = tk.E)
 					
+					ws_right = butn.winfo_reqwidth()
 				else:
-					butn.config(command = lambda: self.vilken_direction('03'))
-					butn.grid(column=0, row=3, sticky = tk.S, columnspan = 2, ipadx = 47)
+					butn.config(command = lambda: self.vilken_direction(DOWN))
+					butn.grid(column=0, row=3, sticky = tk.S, columnspan = 2)
 					
 				
 				zo.a["%0.2d"%(ind)] = butn
-				
-		
-		
-	def set_up_button (self, frame, conf):
-		# The UP-button
-		zo.up_button = tk.Button(frame, text = " /\ ", command = lambda: self.vilken_direction('up'), fg = 'yellow', bg = UP_COLOR)
-		zo.up_button.pack(side = tk.TOP, expand = 1, fill = tk.X)
-		
-	def set_down_button (self, frame, conf):
-	# The DOWN-button
-		zo.down_button = tk.Button (frame, text = " \/", command = lambda: self.vilken_direction('down'), fg = 'blue', bg = DOWN_COLOR)
-		zo.down_button.pack(side = tk.BOTTOM, expand = 1, fill = tk.X, anchor = tk.S)
-
-	def set_right_button (self, frame, conf):	
-		# The RIGHT-button
-		zo.right_button = tk.Button (frame, text = "-->", command = lambda: self.vilken_direction('right'), fg = 'red', bg = RIGHT_COLOR)
-		zo.right_button.pack(side = tk.RIGHT)
-		
-	def set_left_button (self, frame, conf):
-		# The LEFT-button
-		zo.left_button = tk.Button (frame, text = "<--", command = lambda: self.vilken_direction('left'), fg = 'green', bg = LEFT_COLOR)
-		zo.left_button.pack(side = tk.LEFT)
+		# do UP and DOWN buttons twice as long as LEFT and RIGHT buttons		
+		zo.a['00'].grid(ipadx = 2*(ws_right-ws_up+0.5))
+		zo.a['03'].grid(ipadx = 2*(ws_right-ws_up+0.5))
 
 	
 	def vilken_direction(self, vart):
-			
 		zo.the_way_to_go.append(vart)
 		
-		print ('vilken_direction ', str(vart))
 		
 	def start_begin_now(self):
-				
-		nn = sorted(zo.a.keys())
 		numb = len (zo.the_way_to_go)
-		
-		print ("lengd", numb)
 		
 		for n in range(numb):
 			lbbg = zo.a[zo.the_way_to_go[n]].cget("bg")
-			if zo.the_way_to_go[n] == UP :
-				print ('Hej 2! ', str(n))
-				zo.a[zo.the_way_to_go[n]].config(bg = BLINK_COLOR)
-				time.sleep(0.5)
-				zo.a[zo.the_way_to_go[n]].config(bg = lbbg)
-				
-			elif zo.the_way_to_go[n] == DOWN :
-				print ('Hej 3! ', str(n))
-				zo.a[zo.the_way_to_go[n]].config(bg = BLINK_COLOR)
-				time.sleep(0.5)			
-				zo.a[zo.the_way_to_go[n]].configure(bg = lbbg)
-				
-			elif zo.the_way_to_go[n] == LEFT : 
-				print ('Hej 4! ', str(n))
-				zo.a[zo.the_way_to_go[n]].config(bg = BLINK_COLOR)
-				time.sleep(0.5)
-				zo.a[zo.the_way_to_go[n]].configure(bg = lbbg)
-				
-			elif zo.the_way_to_go[n] == RIGHT :
-				print ('Hej 5! ', str(n))
-				zo.a[zo.the_way_to_go[n]].config(bg = BLINK_COLOR)	
-				time.sleep(0.5)
-				zo.a[zo.the_way_to_go[n]].configure(bg = lbbg)
-				
-			else :
-				print ('Hej fel! ', str(n))
-						
-			
+			zo.a[zo.the_way_to_go[n]].config(bg = BLINK_COLOR)
 			time.sleep(0.5)
-
+			zo.a[zo.the_way_to_go[n]].config(bg = lbbg)
+			time.sleep(0.5)
 		
 		
 	def begin_now(self):
 		_thread.start_new_thread(App.start_begin_now,(None,))
 		
 	def reset_now(self):
-				
 		zo.the_way_to_go = []
-		print ('reset_now ', str(zo.the_way_to_go))
+		
 
 
 
